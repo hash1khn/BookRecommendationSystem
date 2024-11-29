b8e659c48838efecc388beee4ce6d83e4c185a30
Hashir Khan <hashirahmedkhan123@gmail.com>
2024-11-29 19:10:07 +0500

    update

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 import random  # For random user selection
 
 # Load datdhdhdbdbdbfbd
-def load_data();
+def load_data():
     books = pd.read_csv(
         'Books.csv',
         encoding='latin1',
